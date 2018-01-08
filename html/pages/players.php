790d93b495ecc375ea0518a5f43f2944daa9720b
Matthijs Kuiper <info@matthijskuiper.nl>
2018-01-08 01:16:20 +0100

    Main pages markup

--- html/pages/players.php
+++ html/pages/players.php
@@ -1,28 +1,26 @@
 <?php
 function InvertSort($curr_field, $filter, $sort) {
-	if ($curr_field != $filter) return(($curr_field == "name") ? "ASC" : "DESC");
-	if ($sort == 'ASC') return('DESC');
-	return('ASC');
+  if ($curr_field != $filter) return(($curr_field == "name") ? "ASC" : "DESC");
+  if ($sort == 'ASC') return('DESC');
+  return('ASC');
 }
 
 function SortPic($curr_field, $filter, $sort) {
-	if ($curr_field != $filter) return;
-	$fname = 'images/s_'. strtolower($sort) .'.png';
-	if (!file_exists($fname)) return;
-	return('&nbsp;<img src="'. $fname .'" border="0" width="11" height="9" alt="" title="('.strtolower($sort).'ending)">');
+  if ($curr_field != $filter) return;
+  $fname = 'images/s_'. strtolower($sort) .'.png';
+  if (!file_exists($fname)) return;
+  return('&nbsp;<img src="'. $fname .'" border="0" width="11" height="9" alt="" class="tooltip" title="('.strtolower($sort).'ending)">');
 }
-
 
 // Get filter and set sorting
 $filter = my_addslashes($_GET[filter]);
 $sort = my_addslashes($_GET[sort]);
 
-IF (empty($filter) or (!in_array(strtolower($filter), array("name", "games", "gamescore", "frags", "kills", "deaths", "suicides", "eff", "accuracy", "ttl", "gametime")))) {
-	$filter = "name";
+if (empty($filter) or (!in_array(strtolower($filter), array("name", "games", "gamescore", "frags", "kills", "deaths", "suicides", "eff", "accuracy", "ttl", "gametime")))) {
+  $filter = "name";
 }
 
 if (empty($sort) or ($sort != 'ASC' and $sort != 'DESC')) $sort = ($filter == "name") ? "ASC" : "DESC";
-
 
 // Work out Prev, Next, First, Last Stuff
 
@@ -32,16 +30,16 @@ $pcount = $r_pcount['pcount'];
 $ecount = $pcount/50;
 $ecount2 = number_format($ecount, 0, '.', '');
 
-IF($ecount > $ecount2) {
-	$ecount2 = $ecount2+1;
+if ($ecount > $ecount2) {
+  $ecount2 = $ecount2+1;
 }
 
 $fpage = 0;
-IF($ecount < 1) { $lpage = 0; }
+if ($ecount < 1) { $lpage = 0; }
 else { $lpage = $ecount2-1; }
 
 $cpage = preg_replace('/\D/', '', $_GET["page"]);
-IF ($cpage == "") { $cpage = "0"; }
+if ($cpage == "") { $cpage = "0"; }
 $qpage = $cpage*50;
 
 $tfpage = $cpage+1;
@@ -49,76 +47,89 @@ $tlpage = $lpage+1;
 
 $ppage = $cpage-1;
 $ppageurl = "<a class=\"pages\" href=\"./?p=players&amp;filter=$filter&amp;sort=$sort&amp;page=$ppage\">[Previous]</a>";
-IF ($ppage < "0") { $ppageurl = "[Previous]"; }
+
+if ($ppage < "0") { $ppageurl = "[Previous]"; }
 
 $npage = $cpage+1;
 $npageurl = "<a class=\"pages\" href=\"./?p=players&amp;filter=$filter&amp;sort=$sort&amp;page=$npage\">[Next]</a>";
-IF ($npage >= "$ecount") { $npageurl = "[Next]"; }
+if ($npage >= "$ecount") { $npageurl = "[Next]"; }
 
 $fpageurl = "<a class=\"pages\" href=\"./?p=players&amp;filter=$filter&amp;sort=$sort&amp;page=$fpage\">[First]</a>";
-IF ($cpage == "0") { $fpageurl = "[First]"; }
+if ($cpage == "0") { $fpageurl = "[First]"; }
 
 $lpageurl = "<a class=\"pages\" href=\"./?p=players&amp;filter=$filter&amp;sort=$sort&amp;page=$lpage\">[Last]</a>";
-IF ($cpage == "$lpage") { $lpageurl = "[Last]"; }
+if ($cpage == "$lpage") { $lpageurl = "[Last]"; }
 
 // Show information
 echo'
-<form NAME="playersearch" METHOD="post" ACTION="./?p=psearch">
-<div class="pages"><b>Page ['.$tfpage.'/'.$tlpage.'] Selection: '.$fpageurl.' / '.$ppageurl.' / '.$npageurl.' / '.$lpageurl.'</b></div>
-<div class="opnote">* Click headings to change Sorting *</div>
-<table class="box" border="0" cellpadding="1" cellspacing="1">
-  <tbody><tr>
-    <td class="heading" colspan="12" align="center">Unreal Tournament Player List</td>
-  </tr>
-  <tr>
-    <td class = "smheading" colspan = "12" ALIGN="center">Name Search:
-      <input TYPE="text" NAME="name" MAXLENGTH="35" SIZE="20" CLASS="searchform">
-      <input TYPE="submit" NAME="Default" VALUE="Search" CLASS="searchformb"></td>
-  </tr>
-  <tr>
-    <td class="smheading" align="center" width="150"><a class="smheading" href="./?p=players&amp;filter=name&amp;sort='.InvertSort('name', $filter, $sort).'">Player Name</a>'.SortPic('name', $filter, $sort).'</td>
-    <td class="smheading" align="center" width="45"><a class="smheading" href="./?p=players&amp;filter=games&amp;sort='.InvertSort('games', $filter, $sort).'">Matches</a>'.SortPic('games', $filter, $sort).'</td>
-    <td class="smheading" align="center" width="50"><a class="smheading" href="./?p=players&amp;filter=gamescore&amp;sort='.InvertSort('gamescore', $filter, $sort).'">Score</a>'.SortPic('gamescore', $filter, $sort).'</td>
-    <td class="smheading" align="center" width="50"><a class="smheading" href="./?p=players&amp;filter=frags&amp;sort='.InvertSort('frags', $filter, $sort).'">Frags</a>'.SortPic('frags', $filter, $sort).'</td>
-    <td class="smheading" align="center" width="50"><a class="smheading" href="./?p=players&amp;filter=kills&amp;sort='.InvertSort('kills', $filter, $sort).'">Kills</a>'.SortPic('kills', $filter, $sort).'</td>
-    <td class="smheading" align="center" width="50"><a class="smheading" href="./?p=players&amp;filter=deaths&amp;sort='.InvertSort('deaths', $filter, $sort).'">Deaths</a>'.SortPic('deaths', $filter, $sort).'</td>
-    <td class="smheading" align="center" width="50"><a class="smheading" href="./?p=players&amp;filter=suicides&amp;sort='.InvertSort('suicides', $filter, $sort).'">Suicides</a>'.SortPic('suicides', $filter, $sort).'</td>
-    <td class="smheading" align="center" width="45"><a class="smheading" href="./?p=players&amp;filter=eff&amp;sort='.InvertSort('eff', $filter, $sort).'">Eff.</a>'.SortPic('eff', $filter, $sort).'</td>
-    <td class="smheading" align="center" width="45"><a class="smheading" href="./?p=players&amp;filter=accuracy&amp;sort='.InvertSort('accuracy', $filter, $sort).'">Acc.</a>'.SortPic('accuracy', $filter, $sort).'</td>
-    <td class="smheading" align="center" width="45"><a class="smheading" href="./?p=players&amp;filter=ttl&amp;sort='.InvertSort('ttl', $filter, $sort).'">TTL</a>'.SortPic('ttl', $filter, $sort).'</td>
-    <td class="smheading" align="center" width="45"><a class="smheading" href="./?p=players&amp;filter=gametime&amp;sort='.InvertSort('gametime', $filter, $sort).'">Hours</a>'.SortPic('gametime', $filter, $sort).'</td>
-  </tr>';
+<form name="playersearch" method="post" action="./?p=psearch">
+  <div class="pages">Page ['.$tfpage.'/'.$tlpage.'] Selection: '.$fpageurl.' / '.$ppageurl.' / '.$npageurl.' / '.$lpageurl.'</div>
+
+	<table class="zebra box" border="0" cellpadding="0" cellspacing="0">
+	<thead>
+	  <tr>
+	    <th class="heading" colspan="12" align="center">Unreal Tournament Player List</th>
+	  </tr>
+	  <tr>
+	    <th class="smheading" colspan="12" align="center">
+		    <div class="darksearch">
+		      <span>
+		        <input type="text" class="search square" placeholder="Search player..." name="name" value="'.htmlentities($playername).'">
+		        <input class="searchbutton" type="submit" value="Search">
+		      </span>
+				</div>
+	    </th>
+	  </tr>
+	  <tr>
+	    <th class="smheading" align="center" width="150"><a class="smheading" href="./?p=players&amp;filter=name&amp;sort='.InvertSort('name', $filter, $sort).'">Player Name</a>'.SortPic('name', $filter, $sort).'</th>
+	    <th class="smheading" align="center" width="45"><a class="smheading" href="./?p=players&amp;filter=games&amp;sort='.InvertSort('games', $filter, $sort).'">Matches</a>'.SortPic('games', $filter, $sort).'</th>
+	    <th class="smheading" align="center" width="50"><a class="smheading" href="./?p=players&amp;filter=gamescore&amp;sort='.InvertSort('gamescore', $filter, $sort).'">Score</a>'.SortPic('gamescore', $filter, $sort).'</th>
+	    <th class="smheading" align="center" width="50"><a class="smheading" href="./?p=players&amp;filter=frags&amp;sort='.InvertSort('frags', $filter, $sort).'">Frags</a>'.SortPic('frags', $filter, $sort).'</th>
+	    <th class="smheading" align="center" width="50"><a class="smheading" href="./?p=players&amp;filter=kills&amp;sort='.InvertSort('kills', $filter, $sort).'">Kills</a>'.SortPic('kills', $filter, $sort).'</th>
+	    <th class="smheading" align="center" width="50"><a class="smheading" href="./?p=players&amp;filter=deaths&amp;sort='.InvertSort('deaths', $filter, $sort).'">Deaths</a>'.SortPic('deaths', $filter, $sort).'</th>
+	    <th class="smheading" align="center" width="50"><a class="smheading" href="./?p=players&amp;filter=suicides&amp;sort='.InvertSort('suicides', $filter, $sort).'">Suicides</a>'.SortPic('suicides', $filter, $sort).'</th>
+	    <th class="smheading" align="center" width="45"><a class="smheading" href="./?p=players&amp;filter=eff&amp;sort='.InvertSort('eff', $filter, $sort).'">Eff.</a>'.SortPic('eff', $filter, $sort).'</th>
+	    <th class="smheading" align="center" width="45"><a class="smheading" href="./?p=players&amp;filter=accuracy&amp;sort='.InvertSort('accuracy', $filter, $sort).'">Acc.</a>'.SortPic('accuracy', $filter, $sort).'</th>
+	    <th class="smheading" align="center" width="45"><a class="smheading" href="./?p=players&amp;filter=ttl&amp;sort='.InvertSort('ttl', $filter, $sort).'">TTL</a>'.SortPic('ttl', $filter, $sort).'</th>
+	    <th class="smheading" align="center" width="45"><a class="smheading" href="./?p=players&amp;filter=gametime&amp;sort='.InvertSort('gametime', $filter, $sort).'">Hours</a>'.SortPic('gametime', $filter, $sort).'</th>
+	  </tr>
+	</thead>
+	<tbody>
+	';
 
 $sql_plist = "SELECT pi.name AS name, pi.country AS country, p.pid, COUNT(p.id) AS games, SUM(p.gamescore) as gamescore, SUM(p.frags) AS frags, SUM(p.kills) AS kills,
 SUM(p.deaths) AS deaths, SUM(p.suicides) as suicides, AVG(p.eff) AS eff, AVG(p.accuracy) AS accuracy, AVG(p.ttl) AS ttl, SUM(gametime) as gametime
 FROM uts_player AS p, uts_pinfo AS pi WHERE p.pid = pi.id AND pi.banned <> 'Y' GROUP BY p.pid ORDER BY $filter $sort LIMIT $qpage,50";
 $q_plist = mysql_query($sql_plist) or die(mysql_error());
+
 while ($r_plist = mysql_fetch_array($q_plist)) {
+  $gametime = sec2hour($r_plist[gametime]);
+  $eff = get_dp($r_plist[eff]);
+  $acc = get_dp($r_plist[accuracy]);
+  $ttl = GetMinutes($r_plist[ttl]);
+  $r_pname = $r_plist[name];
+  $myurl = urlencode($r_pname);
 
-	  $gametime = sec2hour($r_plist[gametime]);
-	  $eff = get_dp($r_plist[eff]);
-	  $acc = get_dp($r_plist[accuracy]);
-	  $ttl = GetMinutes($r_plist[ttl]);
-	  $r_pname = $r_plist[name];
-	  $myurl = urlencode($r_pname);
-
-	  echo'
-	  <tr>
-		<td nowrap class="dark" align="left"><a class="darkhuman" href="./?p=pinfo&amp;pid='.$r_plist['pid'].'">'.FormatPlayerName($r_plist[country], $r_plist['pid'], $r_pname).'</a></td>
-		<td class="grey" align="center">'.$r_plist[games].'</td>
-		<td class="grey" align="center">'.$r_plist[gamescore].'</td>
-		<td class="grey" align="center">'.$r_plist[frags].'</td>
-		<td class="grey" align="center">'.$r_plist[kills].'</td>
-		<td class="grey" align="center">'.$r_plist[deaths].'</td>
-		<td class="grey" align="center">'.$r_plist[suicides].'</td>
-		<td class="grey" align="center">'.$eff.'</td>
-		<td class="grey" align="center">'.$acc.'</td>
-		<td class="grey" align="center">'.$ttl.'</td>
-		<td class="grey" align="center">'.$gametime.'</td>
-	  </tr>';
+  echo'
+  <tr class="zebra clickableRow" href="./?p=pinfo&amp;pid='.$r_plist['pid'].'">
+    <td nowrap align="left">
+      <a href="./?p=pinfo&amp;pid='.$r_plist['pid'].'">'.FormatPlayerName($r_plist[country], $r_plist['pid'], $r_pname).'</a>
+    </td>
+    <td align="center">'.$r_plist[games].'</td>
+    <td align="center">'.$r_plist[gamescore].'</td>
+    <td align="center">'.$r_plist[frags].'</td>
+    <td align="center">'.$r_plist[kills].'</td>
+    <td align="center">'.$r_plist[deaths].'</td>
+    <td align="center">'.$r_plist[suicides].'</td>
+    <td align="center">'.$eff.'</td>
+    <td align="center">'.$acc.'</td>
+    <td align="center">'.$ttl.'</td>
+    <td align="center">'.$gametime.'</td>
+  </tr>';
 }
+
 echo'
 </tbody></table>
-<div class="pages"><b>Page ['.$tfpage.'/'.$tlpage.'] Selection: '.$fpageurl.' / '.$ppageurl.' / '.$npageurl.' / '.$lpageurl.'</b></div>
+<div class="pages">Page ['.$tfpage.'/'.$tlpage.'] Selection: '.$fpageurl.' / '.$ppageurl.' / '.$npageurl.' / '.$lpageurl.'</div>
 </form>';
 ?>
--- html/pages/recent.php
+++ html/pages/recent.php
@@ -1,4 +1,5 @@
 <?php
+
 // Firstly we need to work out First Last Next Prev pages
 $where = ' ';
 $year = !empty($_REQUEST['year']) ? my_addslashes(sprintf("%04d", $_REQUEST['year'])) : 0;
@@ -16,16 +17,16 @@ $mcount = $r_mcount['result'];
 $ecount = $mcount/25;
 $ecount2 = number_format($ecount, 0, '.', '');
 
-IF($ecount > $ecount2) {
-	$ecount2 = $ecount2+1;
+if ($ecount > $ecount2) {
+  $ecount2 = $ecount2+1;
 }
 
 $fpage = 0;
-IF($ecount < 1) { $lpage = 0; }
+if ($ecount < 1) { $lpage = 0; }
 else { $lpage = $ecount2-1; }
 
-$cpage = preg_replace('/\D/', '', $_REQUEST["page"]);
-IF ($cpage == "") { $cpage = "0"; }
+$cpage = mysql_real_escape_string(preg_replace('/\D/', '', $_REQUEST["page"]));
+if ($cpage == "") { $cpage = "0"; }
 $qpage = $cpage*25;
 
 $tfpage = $cpage+1;
@@ -33,105 +34,132 @@ $tlpage = $lpage+1;
 
 $ppage = $cpage-1;
 $ppageurl = "<a class=\"pages\" href=\"./?p=recent&amp;year=$year&amp;month=$month&amp;day=$day&amp;gid=$gid&amp;page=$ppage\">[Previous]</a>";
-IF ($ppage < "0") { $ppageurl = "[Previous]"; }
+if ($ppage < "0") { $ppageurl = "[Previous]"; }
 
 $npage = $cpage+1;
 $npageurl = "<a class=\"pages\" href=\"./?p=recent&amp;year=$year&amp;month=$month&amp;day=$day&amp;gid=$gid&amp;page=$npage\">[Next]</a>";
-IF ($npage >= "$ecount") { $npageurl = "[Next]"; }
+if ($npage >= "$ecount") { $npageurl = "[Next]"; }
 
 $fpageurl = "<a class=\"pages\" href=\"./?p=recent&amp;year=$year&amp;month=$month&amp;day=$day&amp;gid=$gid&amp;page=$fpage\">[First]</a>";
-IF ($cpage == "0") { $fpageurl = "[First]"; }
+if ($cpage == "0") { $fpageurl = "[First]"; }
 
 $lpageurl = "<a class=\"pages\" href=\"./?p=recent&amp;year=$year&amp;month=$month&amp;day=$day&amp;gid=$gid&amp;page=$lpage\">[Last]</a>";
-IF ($cpage == "$lpage") { $lpageurl = "[Last]"; }
+if ($cpage == "$lpage") { $lpageurl = "[Last]"; }
 
 echo '
 <form action="'.$_SERVER['PHP_SELF'].'" method="GET">
-<div class="pages"><b>Page ['.$tfpage.'/'.$tlpage.'] Selection: '.$fpageurl.' / '.$ppageurl.' / '.$npageurl.' / '.$lpageurl.'</b></div>
-<table width="720" class="box" border="0" cellpadding="1" cellspacing="1">
-  <tbody><tr>
-    <td class="heading" colspan="7" align="center">Unreal Tournament Match List</td>
-  </tr>';
+<div class="pages spacer">Page ['.$tfpage.'/'.$tlpage.'] Selection: '.$fpageurl.' / '.$ppageurl.' / '.$npageurl.' / '.$lpageurl.'</div>
 
-echo '
+<table width="900" class="zebra box" border="0" cellpadding="0" cellspacing="0">
+  <tbody><tr>
+    <th class="heading" colspan="7" align="center">Unreal Tournament Match List</th>
+  </tr>
   <tr>
-    <td class = "smheading" colspan = "7" align = "center">';
-echo '<input type="hidden" name="p" value="'.$_REQUEST['p'].'">';
-echo '<table width="700" class="searchform" border="0" cellpadding="1" cellspacing="1">';
-echo '<tr><td><strong>Filter:</strong></td>';
+    <th colspan="7" align="center">';
+echo '<input type="hidden" name="p" value="'.htmlentities($_REQUEST['p']).'">';
+echo '<table width="900" class="smheading" border="0" cellpadding="0" cellspacing="0">';
+echo '<tr><th class="noborders">Filter:</th>';
 //echo '<td>Date:</td>';
-echo '<td><select class="searchform" name="year">';
+echo '<th class="noborders"><select class="searchform" name="year">';
 echo '<option value="0">*</option>';
-for($i = date('Y');$i >= date("Y") - 5; $i--) {
-	$selected = ($year == $i) ? 'selected' : '';
-	echo '<option '.$selected.' value="'.$i.'">'.$i.'</option>';
+
+for ($i = date('Y');$i >= date("Y") - 5; $i--) {
+  $selected = ($year == $i) ? 'selected' : '';
+  echo '<option '.$selected.' value="'.$i.'">'.$i.'</option>';
 }
+
 echo '</select>';
 echo '&nbsp;';
 echo '<select class="searchform" name="month">';
 echo '<option value="0">*</option>';
+
 $monthname = array('', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
-for($i = 1;$i <= 12; $i++) {
-	$selected = ($month == $i) ? 'selected' : '';
-	echo '<option '.$selected.' value="'.$i.'">'.$monthname[$i].'</option>';
+for ($i = 1;$i <= 12; $i++) {
+  $selected = ($month == $i) ? 'selected' : '';
+  echo '<option '.$selected.' value="'.$i.'">'.$monthname[$i].'</option>';
 }
+
 echo '</select>';
 echo '&nbsp;';
 echo '<select class="searchform" name="day">';
 echo '<option value="0">*</option>';
-for($i = 1;$i <= 31; $i++) {
-	$selected = ($day == $i) ? 'selected' : '';
-	echo '<option '.$selected.' value="'.$i.'">'.$i.'</option>';
-}
-echo '</select></td>';
 
-echo '<td>Gametype:</td>';
-echo '<td><select class="searchform" name="gid">';
+for ($i = 1;$i <= 31; $i++) {
+  $selected = ($day == $i) ? 'selected' : '';
+  echo '<option '.$selected.' value="'.$i.'">'.$i.'</option>';
+}
+
+echo '</select></th>';
+echo '<th class="noborders">Gametype:</th>';
+echo '<th class="noborders"><select class="searchform" name="gid">';
 echo '<option value="0">*</option>';
+
 $sql_game = "SELECT DISTINCT(p.gid), g.name FROM uts_player AS p, uts_games AS g WHERE p.gid = g.id ORDER BY g.name ASC";
 $q_game = mysql_query($sql_game) or die(mysql_error());
+
 while ($r_game = mysql_fetch_array($q_game)) {
-	$selected = ($r_game['gid'] == $gid) ? 'selected' : '';
-	echo '<option '.$selected.' value="'.$r_game['gid'].'">'. $r_game['name'] .'</option>';
+  $selected = ($r_game['gid'] == $gid) ? 'selected' : '';
+  echo '<option '.$selected.' value="'.$r_game['gid'].'">'. $r_game['name'] .'</option>';
 }
-echo '</select></td>';
-echo '<td><input class="searchform" type="Submit" name="filter" value="Apply"></td>';
+
+echo '</select></th>';
+echo '<th class="noborders"><input class="searchform" type="Submit" name="filter" value="Apply"></th>';
 echo '</tr></table>';
 echo '</td></tr>';
-
 echo '
   <tr>
-    <td class="smheading" align="center" width="80">Match ID</td>
-    <td class="smheading" align="center" width="220">Date/Time</td>
-    <td class="smheading" align="center" width="140">Match Type</td>
-    <td class="smheading" align="center">Map</td>
-    <td class="smheading" align="center" width="60">Time</td>
-    <td class="smheading" align="center" width="60">Players</td>
+    <th class="smheading" align="center" width="40">ID</th>
+    <th class="smheading" align="center" width="220">Date/Time</th>
+    <th class="smheading" align="center" width="140">Match Type</th>
+    <th class="smheading" align="center">Map</th>
+	  <th class="smheading" align="center" width="200">Scores</th>
   </tr>';
 
-$sql_recent = "SELECT m.id, m.time, g.name AS gamename, m.mapfile, m.gametime, (SELECT count(p.id) FROM uts_player AS p WHERE m.id = p.matchid) as players FROM uts_match AS m, uts_games AS g WHERE g.id = m.gid $where ORDER BY m.time DESC LIMIT $qpage,25";
+$sql_recent = "SELECT m.id, m.time, g.name AS gamename, m.mapfile, m.gametime, t0score, t1score, t2score, t3score, (SELECT count(p.id) FROM uts_player AS p WHERE m.id = p.matchid) as players FROM uts_match AS m, uts_games AS g WHERE g.id = m.gid $where ORDER BY m.time DESC LIMIT ".mysql_real_escape_string($qpage).",50";
 $q_recent = mysql_query($sql_recent) or die(mysql_error());
+
 while ($r_recent = mysql_fetch_array($q_recent)) {
+  $r_time = mdate($r_recent[time]);
+  $r_mapfile = un_ut($r_recent[mapfile]);
+  $r_gametime = GetMinutes($r_recent[gametime]);
+  $winner = max($r_recent[t0score], $r_recent[t1score], $r_recent[t2score], $r_recent[t3score]);
+  $moreThan2Teams = ($r_recent[t2score]!=0);
 
-	  $r_time = mdate($r_recent[time]);
-	  $r_mapfile = un_ut($r_recent[mapfile]);
-	  $r_gametime = GetMinutes($r_recent[gametime]);
+  if ($winner == $r_recent[t0score]) {
+    $winnercolor = "red";
+    $winmsg = "Red is the winner!";
+  }
+  elseif ($winner == $r_recent[t1score]) {
+    $winnercolor = "blue";
+    $winmsg = "Blue is the winner!";
+  }
+  elseif ($winner == $r_recent[t2score]) {
+    $winnercolor = "green";
+    $winmsg = "Green is the winner!";
+  }
+  else {
+    $winnercolor = "gold";
+    $winmsg = "Gold is the winner!";
+  };
 
-	  echo'
-	  <tr>
-		<td class="dark" align="center"><a class="darkid" href="./?p=match&amp;mid='.$r_recent[id].'">'.$r_recent[id].'</a></td>
-		<td nowrap class="dark" align="center"><a class="darkhuman" href="./?p=match&amp;mid='.$r_recent[id].'">'.$r_time.'</a></td>
-		<td nowrap class="grey" align="center">'.$r_recent[gamename].'</td>
-		<td class="grey" align="center">'.$r_mapfile.'</td>
-		<td class="grey" align="center">'.$r_gametime.'</td>
-		<td class="grey" align="center">'.$r_recent[players].'</td>
-	  </tr>';
+  echo '
+  <tr class="clickableRow" href="./?p=match&amp;mid='.$r_recent[id].'">
+    <td align="center">'.$r_recent[id].'</td>
+    <td nowrap align="center"><a href="./?p=match&amp;mid='.$r_recent[id].'">'.$r_time.'</a></td>
+    <td nowrap align="center">'.$r_recent[gamename].'</td>
+    <td align="center">'.$r_mapfile.'</td>
+    <td class="tooltip" title="'.$winmsg.'" align="center"><span class="redbox">'.$r_recent[t0score].'</span><span class="bluebox">'.$r_recent[t1score].'</span>';
+
+		if  ($moreThan2Teams) {
+			echo '<span class="greenbox">'.$r_recent[t2score].' </span><span class="goldbox">  '.$r_recent[t3score].' </span>';
+		}
+
+    '</td>
+  </tr>';
 }
-
-
 
 echo '
 </tbody></table>
-<div class="pages"><b>Page ['.$tfpage.'/'.$tlpage.'] Selection: '.$fpageurl.' / '.$ppageurl.' / '.$npageurl.' / '.$lpageurl.'</b></div>
+<div class="pages">Page ['.$tfpage.'/'.$tlpage.'] Selection: '.$fpageurl.' / '.$ppageurl.' / '.$npageurl.' / '.$lpageurl.'</div>
 </form>';
 ?>

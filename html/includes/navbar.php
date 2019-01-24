36c2336f8efdc569869035455120d392e7ee2cbe
beatzz <elsheepo@gmail.com>
2019-01-24 19:38:12 +0900

    Responsive BS4 navbar...done :sheep:

--- html/includes/navbar.php
+++ html/includes/navbar.php
@@ -1,22 +1,25 @@
 
-  <nav class="navbar">
-    <ul>
-      <li class="logo">
-        <a href="./">
-          <img src="assets/images/navbar-logo.png" alt="UTStats Logo">
-        </a>
-      </li>
-      <li>
-        <a href="./?p=recent">Recent</a>
-      </li>
-      <li>
-        <a href="./?p=rank">Rankings</a>
-      </li>
-      <li>
-        <a href="./?p=players">Players</a>
-      </li>
-      <li>
-        <a href="./?p=watchlist">Watchlist</a>
-      </li>
-    </ul>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="navbar-brand" href="./">
+        <img src="assets/images/navbar-logo.png" class="logo" alt="UTStats Logo">
+      </a>
+      <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="./?p=recent">Recent</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./?p=rank">Rankings</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./?p=players">Players</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./?p=watchlist">Watchlist</a>
+        </li>
+      </ul>
+    </div>
   </nav>

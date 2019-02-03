af47c4f7b08ef6ea796615e9868c62d74e86b8bf
beatzz <elsheepo@gmail.com>
2019-02-03 15:43:40 +0900

    fresh start on the UTStats modernization :sheep:
    1. Applying the responsive, mobile friendly Boostrap 4 CSS
        framework.
    2. Editing PHP echo statements to output properly formatted
        HTML.
    3. Migrate all mysqli calls to PDOs for SQL Injection security.

--- html/includes/footer.php
+++ html/includes/footer.php
@@ -1,17 +1,43 @@
-    <div class="footer">
-      <a href="./?p=totals">Totals</a>
-      <a href="./?p=maps">Maps</a>
-      <a href="./?p=servers">Servers</a>
-      <a href="./import.php">Import</a>
-      <a href="./admin.php">Admin</a>
-      <a href="./?p=credits">Credits</a>
-      <a href="./?p=help">Help</a>
-      <br>
-      <br>
-      <a href="https://github.com/sn3p/utstats" target="_blank">UTStats Beta 4.4.1</a>
-      &copy; <?php echo date("Y"); ?> azazel, AnthraX toa and sn3p
+
+      </div>
+      <div class="col"></div>
     </div>
-  </body>
+  </div>
+  <div class="phantom"></div>
+  <footer class="footer">
+    <div class ="container">
+    <a href="./?p=totals">Totals</a>
+    <a href="./?p=maps">Maps</a>
+    <a href="./?p=servers">Servers</a>
+    <a href="./import.php">Import</a>
+    <a href="./admin.php">Admin</a>
+    <a href="./?p=credits">Credits</a>
+    <a href="./?p=help">Help</a>
+    <br />
+    <br />
+    <a href="https://github.com/sn3p/utstats" target="_blank">UTStats Beta 4.4.1</a>
+    &copy; <?php echo date("Y"); ?> azazel, AnthraX, toa, <a href="https://github.com/elsheepo" rel="noopener noreferrer" target="_blank" style="margin-left:0">elsheepo</a> and sn3p
+    </div>
+  </footer>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <!-- <script type="text/javascript" src="assets/lib/jquery-1.11.1.min.js"></script> -->
+  <script type="text/javascript">
+    <!--
+    var ol_fgclass="dark"; var ol_bgclass="darkbox"; var ol_textfontclass="dark"; var ol_captionfontclass="hlheading";
+    -->
+  </script>
+  <script type="text/javascript" src="assets/lib/overlib_mini.js"><!-- overLIB (c) Erik Bosrup --></script>
+  <script type="text/javascript" src="assets/lib/highcharts/adapters/standalone-framework.js"></script>
+  <script type="text/javascript" src="assets/lib/highcharts/highcharts.js"></script>
+  <script type="text/javascript" src="assets/lib/highcharts/highcharts-more.js"></script>
+  <script type="text/javascript" src="assets/lib/highcharts/highcharts-functions.js"></script>
+  <script type="text/javascript" src="assets/lib/highcharts/themes/utstats.js"></script>
+  <script type="text/javascript" src="assets/lib/tooltipster/jquery.tooltipster.min.js"></script>
+  <script type="text/javascript" src="assets/main.js"></script>
+</body>
 </html>
 
 <?php ob_end_flush(); ?>
--- html/includes/navbar.php
+++ html/includes/navbar.php
@@ -1,21 +1,31 @@
-<div class="navbar">
-  <ul>
-    <li class="logo">
-      <a href="./">
-        <img src="assets/images/navbar-logo.png" alt="UTStats Logo">
+
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="./">
+        <img src="assets/images/navbar-logo.png" class="logo" alt="UTStats Logo">
       </a>
-    </li>
-    <li>
-      <a href="./?p=recent">Recent</a>
-    </li>
-    <li>
-      <a href="./?p=rank">Rankings</a>
-    </li>
-    <li>
-      <a href="./?p=players">Players</a>
-    </li>
-    <li>
-      <a href="./?p=watchlist">Watchlist</a>
-    </li>
-  </ul>
-</div>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="./?p=recent">Recent</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./?p=rank">Rankings</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./?p=players">Players</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./?p=watchlist">Watchlist</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col"></div>
+        <div class="col-sm-12 col-lg-8 justify-content-center align-items-center main-content">
